2c5a0658f4e35749c107b858f3709817849eac18
mariaCalvario <83970969+mariaCalvario@users.noreply.github.com>
2021-07-29 18:10:13 +0100

    Fixed an error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
