89639eb465f95e0793ac0f57109ae1dd5a72551d
mariaCalvario <83970969+mariaCalvario@users.noreply.github.com>
2021-07-30 12:04:42 +0100

    Added a search engine in a new Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from "./logo.svg";
+import Dictionary from "./Dictionary";
+
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <Dictionary />
+        <footer className="App-footer">
+          <small>Coded by Maria Calvário</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
