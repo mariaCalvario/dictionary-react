7670693e4b8897ee2b62a1f4b2d78218e5347e8f
mariaCalvario <83970969+mariaCalvario@users.noreply.github.com>
2021-08-04 15:26:49 +0100

    Added audio controls to phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -6,10 +6,11 @@ export default function Phonetics(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetics">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
       <span className="text">{props.phonetic.text}</span>
+      <br />
+      <audio controls src={props.phonetic.audio}>
+        Listen
+      </audio>
     </div>
   );
 }
